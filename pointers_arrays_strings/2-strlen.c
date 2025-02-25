d5cc3d88bb296d41ea965290d658fd9fcb9f85d0
Sunny Pritchard <pritchardsunny@gmail.com>
2025-02-25 16:05:34 +0000

    Return strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - return the lenght of a string
+ * @s: char s
+ *
+ *
+ * Return: 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
