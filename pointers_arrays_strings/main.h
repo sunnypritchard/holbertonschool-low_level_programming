ba988dd04c66211973c68ba467c782bd86559191
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-02 20:32:56 +0000

    Prints n elements of an array of int

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -20,5 +22,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-
+void print_array(int *a, int n);
 #endif
