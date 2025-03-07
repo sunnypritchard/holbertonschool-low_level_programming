b037548369a8a870d4e2f866e1b44f12370a7c98
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-07 02:14:55 +0100

    add two integers

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add -  adds two integers and returns the result.
  * @num1: int num1
- * @num2 int num2
+ * @num2: int num2
  *
  * Description: adds two integers and returns the result.
  *
