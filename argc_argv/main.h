96fa9a05b44fc6b28796b035443d0c8fc8870684
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-11 11:24:17 +0100

    program prints it name

---
+++ argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* C - argc, argv */
+int _putchar(char c);
+
+#endif
---
+++ argc_argv/note/argc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+	int i;
+
+	// Print the element(s) of argc
+	printf ("argc = %d\n", argc);
+	printf("%s", argv[argc]);
+
+	return 0;
+}
