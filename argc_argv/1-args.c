4e7ed8197d96bec668367a8fe90e07e2e678737c
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-11 12:49:42 +0100

    print the number argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector (element)
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	
+	printf("%d\n", argc - 1);
+	return (0);
+}
