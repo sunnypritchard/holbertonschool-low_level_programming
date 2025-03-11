481394f8fc3a50fa00d6717f21d169671301cd11
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-11 14:38:52 +0100

    print the product of two cmd argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector (element)
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int operand1, operand2, product;
+
+	if (argc > 0)
+	{
+		if (argc < 3)
+		{
+			printf("Error\n");
+			return (0);
+		}
+
+		operand1 =  atoi(argv[1]);
+		operand2 =  atoi(argv[1]);
+		product = operand1 * operand2;
+
+		printf("%d\n", product);
+
+	}
+	return (0);
+}
