5f63e19a5bbcf1d394ca3925b811f17b685a4949
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-12 12:23:07 +0100

    Create and arr of char

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array to create
+ * @c: char to initialize the array c
+ *
+ * Return: pointer to the array (Success), NULL (Error)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr_arr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr_arr = malloc(sizeof(char) * size);
+
+	if (ptr_arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		ptr_arr[i] = c;
+	}
+	return (ptr_arr);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* C - malloc, free */
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
+
+
