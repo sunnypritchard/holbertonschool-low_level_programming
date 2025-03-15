8c1e52df31350b8df6d1e48fabdcd864b925f6f6
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-15 17:06:19 +0100

    Define type struct dog with three elements

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog
+ * @name: dog name
+ * @owner: dog owner
+ * @age: dog age
+ *
+ * Description: Define type  struct  dog with the following members:
+ *
+ * name, owner, and age.
+ */
 
-/* C - Structures, typedef */
-
-
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
 #endif
