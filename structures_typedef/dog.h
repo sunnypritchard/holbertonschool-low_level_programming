66fefcec3e82610656c64663ba5f4c7894e7fa29
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-15 19:15:47 +0100

    Define new type dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -12,13 +12,14 @@
  *
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
