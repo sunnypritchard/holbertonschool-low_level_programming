58ebf6aa073c6e8c306290a508f77c3521373919
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-16 18:28:14 +0100

    create new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -83,6 +83,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	_strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
-
+	if (new_dog->age < 0)
+	{
+	    free(new_dog->owner);
+	    free(new_dog->name);
+	    free(new_dog);
+	    return (NULL);
+	}
+	
 	return (new_dog);
 }
