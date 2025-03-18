34db95b0ff2a5d66aa9929eb4764f58d9f788cee
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-18 10:50:24 +0100

    Arithmetic program

---
+++ function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "calc.h"
+
+/**
+ * main - Entry point of the calculator program.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 0 on success, exits with error codes otherwise.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+	/* handle invalid number of arguments */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	/* get the corresponding function */
+	operation = get_op_func(argv[2]);
+
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "calc.h"
+
+/**
+ * op_add - Adds two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Quotient of a divided by b.
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Finds remainder of division.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Remainder of a divided by b.
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
