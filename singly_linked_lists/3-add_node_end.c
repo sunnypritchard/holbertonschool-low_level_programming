9ad07abf0f3ab7529ef92b6d73f56bea75e3fdd4
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-31 22:04:06 +0200

    refactor(add_node_end): Improve input validation and error handling

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - Calculates the length of a string.
+ * @str: The string whose length is to be calculated.
+ *
+ * Return: The length of the string.
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (str[len] != '\0')
+	len++;
+
+	return (len);
+}
+
+/**
  * add_node_end - Adds a new node at the end of a list_t list.
  * @head: A pointer to the head of the list_t list.
  * @str: The string to be added to the new node.
@@ -14,15 +31,21 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node, *temp;
 	size_t str_len;
 
-	if (head == NULL)
+	if (head == NULL || str == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	str_len = strlen(str);
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	str_len = _strlen(str);
 	new_node->len = str_len;
 	new_node->next = NULL;
 
@@ -35,6 +58,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		temp = *head;
 		while (temp->next != NULL)
 			temp = temp->next;
+
 		temp->next = new_node;
 	}
 
