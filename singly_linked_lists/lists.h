738e8211201986c7984ea8981ee5026c15855977
Sunny Pritchard <pritchardsunny@gmail.com>
2025-03-31 18:18:13 +0200

    feat(list_len): Add function to return the number of elements in a linked list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 /* C Standard Library */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -23,5 +24,6 @@ typedef struct list_s
 
 /* Function Prototype */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
