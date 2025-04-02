147a9095a427e688994968d2e37da963160d4aad
Sunny Pritchard <pritchardsunny@gmail.com>
2025-04-02 14:28:06 +0200

    feat: Add get_dnodeint_at_index function to retrieve a node by index
    - Implemented the `get_dnodeint_at_index` function to return the nth node of a `dlistint_t` list.
    - Returns `NULL` if the specified index does not exist.
    
    Tested with the provided `5-main.c`  to ensure correct retrieval of nodes at various indices.

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node to be returned
+ *
+ * Return: pointer to the nth node, or NULL if it doesn't exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int i = 0;
+
+	while (current != NULL)
+	{
+		if (i == index)
+			return (current);
+		current = current->next;
+		i++;
+	}
+
+	return (NULL);
+}
+
