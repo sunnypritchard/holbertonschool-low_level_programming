e7898593ac8510ae4d40c352d8382677649bb139
Sunny Pritchard <pritchardsunny@gmail.com>
2025-04-09 23:37:06 +0200

    doc: Rename folder with prefix hex

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * @void: No arguments
+ *
+ * Description: Prints a quote to the standard error output
+ *		using the write system call.
+ *
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
