cc828ce0d4b1c7b67c9ecf117dae6f190f565b91
Sunny Pritchard <pritchardsunny@gmail.com>
2025-04-09 23:29:08 +0200

    feat: Add program to print a quote to standard error - Implemented a C program that uses the `write` system call to output   the quote: "and that piece of art is useful\" - Dora Korpar, 2015-10-19"   to the standard error stream. - The program returns an exit status of 1 to indicate a specific condition.
    This demonstrates low-level output operations in C.

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,14 +8,11 @@
  * Description: Prints a quote to the standard error output
  *		using the write system call.
  *
- *
  * Return: Always 1 (Error)
  */
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, str, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
