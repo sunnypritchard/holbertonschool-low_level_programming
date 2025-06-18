bd2e0ab27a1c31135d8da7850ff395dfde789aaf
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-18 08:23:08 +0000

    feat: add program to print lowercase alphabet using putchar
    Implements a simple loop that prints all lowercase letters from 'a' to 'z'
    using a for loop and the standard library function putchar().

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the lowercase alphabet from 'a' to 'z'
+ * using a for loop and the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	return (0);
+}
