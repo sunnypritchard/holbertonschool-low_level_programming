e92c4360b32f42b7868efe613e0223ed378f2504
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-20 12:27:41 +0000

    feat: Add prototype 'int _islower(int c);' to header file main.h

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase
+ * @c: The character to check (as an ASCII value)
+ *
+ * Description: Loops through lowercase alphabet characters to check
+ * if the input character matches any of them. Returns 1 if it is
+ * lowercase, otherwise returns 0.
+ *
+ * Return: 1 if c is a lowercase letter, 0 otherwise
+ */
+int _islower(int c)
+{
+	char lowercase_letter;
+
+	for (lowercase_letter = 'a'; lowercase_letter <= 'z'; lowercase_letter++)
+	{
+		if (c == lowercase_letter)
+			return (1);
+	}
+	return (0);
+}
