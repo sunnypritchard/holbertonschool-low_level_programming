9c5b841e413059e21a3a6f782ed813275d6e6c0b
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-23 13:07:33 +0000

    feat: prints the 9 times table, starting from 0

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ *
+ * Description: Uses nested loops to calculate and print the times table.
+ * Each product is properly formatted with spaces and commas for readability.
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i * j < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(i * j + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(i * j / 10 + '0');
+				_putchar(i * j % 10 + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
