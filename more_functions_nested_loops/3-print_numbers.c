66afd8c321299e9244ce020d3e6a0fd2a3af15e3
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-24 12:50:11 +0000

    feat: prints the numbers, from 0 through 9 follow by a new line

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * print_numbers - Prints the numbers from 0 to 9,
+ *                 followed by a new line.
+ *
+ * Return: void
+ */
 void print_numbers(void)
 {
 	int n;
