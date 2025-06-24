1cc23203e708b8d86e96cc29a4932874e4e7df20
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-24 13:27:53 +0000

    feat: prints the numbers, from 0 to 9, except 2 and 4 follow by new line

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - Prints the numbers from 0 to 9 (except 2 and 4),
+ *                      followed by a new line.
+ *
+ * Return: void
+ */
 void print_most_numbers(void)
 {
 	int n;
