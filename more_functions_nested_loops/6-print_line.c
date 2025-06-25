7cb96674563d598b2f718e523171fab4756d4062
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-25 12:37:40 +0000

    feat: draws a straight line in the terminal

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,15 @@
 void print_line(int n)
 {
 	int i;
-		
-	for (i = 0; i <= n; i++)
+
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
 }
