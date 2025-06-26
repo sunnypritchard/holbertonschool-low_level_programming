a93c57b43a4180b334afa5eff4a4e963ae425e1d
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-26 12:15:20 +0000

    feat: implement FizzBuzz logic from 1 to 100

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers from 1 to 100. For multiples of 3, prints "Fizz"
+ * instead of the number. For multiples of 5, prints "Buzz". For numbers which
+ * are multiples of both 3 and 5, prints "FizzBuzz". Each output is separated
+ * by a space.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz ");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+
+	return (0);
+}
