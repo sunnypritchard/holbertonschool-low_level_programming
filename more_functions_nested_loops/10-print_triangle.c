5eec646e30b6416e458002511a6ed7ed86ac4536
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-27 10:00:24 +0000

    feat: prints triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,13 +16,16 @@ void print_triangle(int size)
 	{
 		_putchar('\n');
 	}
-
 	for (row = 1; row <= size; row++)
 	{
 		for (space = 0; space < size - row; space++)
-		       _putchar(' ');
+		{
+			_putchar(' ');
+		}
 		for (hash = 0; hash < row; hash++)
+		{
 			_putchar('#');
+		}
 		_putchar('\n');
 	}
-}	
+}
