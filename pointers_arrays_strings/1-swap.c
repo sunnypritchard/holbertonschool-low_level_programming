5dd6e742783aba96578b041fd37a7d1af10b8267
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-27 12:02:58 +0000

    test: checks code for the function swap_int(int *a, int *b)

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * Description: Uses a temporary variable to swap the values
+ * of the two integers pointed to by a and b.
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
