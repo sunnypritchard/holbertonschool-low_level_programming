e8583777a1c04f90075cb9875167754e1079ece8
Sunny Pritchard <pritchardsunny@gmail.com>
2025-06-27 11:17:39 +0000

    test: checks code for the function reset_to_98(int *n)

---
+++ pointers_arrays_strings/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+
+	return(0);
+}
