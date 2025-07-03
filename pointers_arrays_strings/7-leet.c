5c7a74f0970512cb7d9a0b37b201f3b0da90a80e
Sunny Pritchard <pritchardsunny@gmail.com>
2025-07-03 21:24:29 +0000

    feat: encodes a string into 1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,13 +11,13 @@ char *leet(char *str)
 	int i, j;
 
 	char letters[] = "aAeEoOtTlL";
-	char numbers[] = "43071";
+	char numbers[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; letters[j] != '\0'; j++)
 		{
-			if (str[i] == letters[i])
+			if (str[i] == letters[j])
 			{
 				str[i] = numbers[j];
 				break;
