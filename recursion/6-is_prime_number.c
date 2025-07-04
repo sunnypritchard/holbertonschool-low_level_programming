551b26eef91e402d1b748c517b72ee353fc8a692
Sunny Pritchard <pritchardsunny@gmail.com>
2025-07-04 20:40:58 +0000

    feat: returns 1 if n is a prime number, otherwise 0

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * check_prime - helper function to check for prime number recursively
+ * @n: number to check
+ * @i: divisor to test
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int check_prime(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (n % 1 == 0)
+		return (0);
+	return (check_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if n is a prime number, otherwise 0
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
+}
