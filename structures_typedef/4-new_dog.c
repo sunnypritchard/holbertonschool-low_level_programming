21577adad4bceff859d633b3aa6b55dc56776b83
Sunny Pritchard <pritcharsunny@gmail.com>
2025-07-11 20:09:58 +0000

    feat: Creates new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the new dog, or NULL if failed
+ *
+ * Description: Allocates memory for a new dog and initializes its fields.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+
+	d->name = name;
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->owner = owner;
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	d->age = age;
+
+	return (d);
+}
