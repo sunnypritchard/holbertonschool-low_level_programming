23dbf10ee3c94328e69dd7e0b00ca3fc8957ee0d
Sunny Pritchard <pritchardsunny@gmail.com>
2025-07-15 13:03:48 +0000

    feat: add print_name prototype

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * File: function_pointers.h
+ * Auth: Sunny Pritchard 
+ * Desc: Header file containing prototypes for all functions
+ *       used in the function_pointers directory.
+ */
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
