faaba248d9da9fd6888b3abf82a772de492141d7
Sunny Pritchard <pritchardsunny@gmail.com>
2025-07-17 13:14:01 +0000

    feat: prints number

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers, separated by a given string
+ * @separator: The string to be printed between numbers
+ * @n: The number of integers passed to the function
+ *
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
