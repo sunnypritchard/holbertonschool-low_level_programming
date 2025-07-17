1900c08401566f8399dda7dc8c80d9348fae867f
root <pritchardsunny@gmail.com>
2025-07-17 12:11:24 +0000

    feat: add prototype int sum_them_all and header variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,9 @@
 #ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
+
 #endif
