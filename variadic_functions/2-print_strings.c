8557ed773df7d717e0bec4286f18837a1157e078
Sunny Pritchard <sunnypritchard@gmail.com>
2025-07-21 22:55:24 +0200

    feat: prints strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,20 +11,20 @@
  */
 void print_strings(const char *separator, unsigned int n, ...)
 {
-	va_list args;
+	va_list ap;
 	unsigned int i;
 	const char *s;
 
-	va_start(args, n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(args, const char *);
+		s = va_arg(ap, const char *);
 		printf("%s", s ? s : "(nil)");
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
 
 	printf("\n");
-	va_end(args);
+	va_end(ap);
 }
