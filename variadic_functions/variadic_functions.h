aa711080324de7ab408656c9dc993a131d90261a
Sunny Pritchard <sunnypritchard@gmail.com>
2025-07-21 08:55:39 +0200

    docs: update header file description

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,11 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
+/*
+ * File: variadic_functions.h
+ * Auth: Sunny Pritchard 
+ * Desc: Header file containing prototypes for all functions
+ *       used in the variadic_functions directory.
+ */
 #include <stdio.h>
 #include <stdarg.h>
 
