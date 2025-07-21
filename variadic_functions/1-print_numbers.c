826cb6e9e85e2fed3b895ce8a2a0da51bc3af8fa
Sunny Pritchard <sunnypritchard@gmail.com>
2025-07-21 11:25:01 +0200

    fix: consistent types for loop variable

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,14 +9,14 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	va_list ap;
 	unsigned int i;
 
-	va_start(args, n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
+		printf("%d", va_arg(ap, int));
 		if (separator && i != n - 1)
 			printf("%s", separator);
 	}
