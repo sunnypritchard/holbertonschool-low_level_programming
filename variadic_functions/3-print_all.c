1b42ba98a9db97beec8e3604ddd96d74abeec252
Sunny Pritchard <sunnypritchard@gmail.com>
2025-07-22 13:19:00 +0200

    refactor: using sizeof for printing array size

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,54 +1,86 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - Prints arguments based on the provided format string.
- * @format: A string containing format specifiers:
- *          'c' for char, 'i' for int, 'f' for float, 's' for string.
+ * print_char - Prints a char argument
+ * @separator: String to print before the argument (e.g. ", ")
+ * @args: Pointer to the va_list containing the arguments
+ */
+static void print_char (const char *separator, va_list *args)
+{
+	printf("%s%c", separator, (char)va_arg(*args, int));
+}
+
+/**
+ * print_int - Prints an int argument
+ * @separator: String to print before the argument
+ * @args: Pointer to the va_list containing the arguments
+ */
+static void print_int(const char *separator, va_list *args)
+{
+	printf("%s%d", separator, va_arg(*args, int));
+}
+
+/**
+ * print_float - Prints a float argument
+ * @separator: String to print before the argument
+ * @args: Pointer to the va_list containing the arguments
+ */
+static void print_float(const char *separator, va_list *args)
+{
+	printf("%s%f", separator, va_arg(*args, double));
+}
+
+/**
+ * print_string - Prints a string argument
+ * @separator: String to print before the argument
+ * @args: Pointer to the va_list containing the arguments
+ */
+static void print_string(const char *separator, va_list *args)
+{
+	char *str;
+	str = va_arg(*args, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s%s", separator, str);
+}
+
+
+/**
+ * print_all - Prints arguments based on a format string
+ * @format: A string containing format specifiers ('c', 'i', 'f', 's')
  *
- * Description: Prints the arguments separated by ", ".
- * If a string is NULL, prints "(nil)" instead.
- * Ends with a newline.
+ * Description: The function prints all arguments separated by ", ",
+ *              according to the specifiers in @format.
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i;
-	char *str;
-	char sep = 0;
+	const format_printer_t printers[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string}
+	};
 
-	va_start(args, format);
+	va_list ap;
+	const char *separator = "";
+	size_t i, j;
+	size_t n_printers = sizeof(printers) / sizeof(printers[0]);
 
-	while (format && format[i])
+	va_start(ap, format);
+
+	for (i = 0; format && format[i]; i++)
 	{
-		if (sep)
-		{
-			printf(", ");
+		for (j = 0; j < n_printers; j++)
+		{	if (format[i] == printers[j].specifier)
+			{
+				printers[j].print_func(separator, &ap);
+				separator = ", ";
+				break;
+			}
 		}
-
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				sep = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				sep = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				sep = 1;
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(nil)";
-				printf("%s", str);
-				sep = 1;
-				break;
-		}
-		i++;
 	}
+
+	va_end(ap);
 	printf("\n");
-	va_end(args);
 }
