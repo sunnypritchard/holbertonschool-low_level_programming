c21f195350f1a8987c95b85c5ff847753ba7df64
Sunny Pritchard <sunnypritchard@gmail.com>
2025-07-22 13:20:50 +0200

    feat: add struct format_printer_s and varaible format_printer_t to header variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,6 +9,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct format_printer_s - Maps a type specifier to its printer function
+ * @specifier: The format specifier character (e.g. 'c', 'i', 'f', 's')
+ * @print_func: Pointer to the function that prints the corresponding argument
+ */
+typedef struct format_printer_s
+{
+    char specifier;
+    void (*print_func)(const char *separator, va_list *ap);
+} format_printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
