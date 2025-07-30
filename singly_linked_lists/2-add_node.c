f561b0f3b0799fbb0e5013a64580754209ec5813
Sunny Pritchard <pritchardsunny@gmail.com>
2025-07-30 10:39:56 +0200

    fix: get string lenght

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,27 +10,25 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *dup_str;
+	unsigned int len = 0;
 
-	if (str == NULL)
-		return (NULL);
-
-	dup_str = strdup(str);
-	if (dup_str == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	while (str[len])
+	{
+		len++;
+	}
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free(dup_str);
+		free(new_node);
 		return (NULL);
 	}
 
-	new_node->str = dup_str;
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
