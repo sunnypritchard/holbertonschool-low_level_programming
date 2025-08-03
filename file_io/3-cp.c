48842a21c1c14fb5c2211f0b62aa0c8b9aea3082
Sunny Pritchard <pritchardsunny@gmail.com>
2025-08-03 17:40:10 +0200

    feat: copies the content of one file to another

---
+++ file_io/3-cp.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * main - copies the content of one file to another
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success, exits with error code otherwise
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+	ssize_t r_bytes, w_bytes;
+	char buffer[BUF_SIZE];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(fd_from);
+		exit(99);
+	}
+
+	while ((r_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		w_bytes = write(fd_to, buffer, r_bytes);
+		if (w_bytes == -1 || w_bytes != r_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (r_bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
